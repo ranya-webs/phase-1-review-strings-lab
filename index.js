5719c3112bca28688d4e75ca99aec2b26b183851
Ranya Al-Jumaili <ranyaosam1@gmail.com>
2022-08-08 17:55:14 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define a string variable, currentUser.
+const currentUser = "Ranya";
+
+//Write a welcome message to the current user of a social network called Flatbook 
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//Write an excited welcome message -> outputs a sentence with all letters being uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//Write a short greeting containing the first letter of the currentUser name
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
